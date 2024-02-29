dd16d86839a3caaa1e422e049cab579dbab04b78
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2024-02-29 18:33:54 +0530

    Create recursion.java

---
+++ 344. Reverse String/recursion.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public static void reverse(int i,int j,char[] s){
+        if(i>j) return;
+        char a=s[i];
+        s[i]=s[j];
+        s[j]=a;
+        i++;j--;
+        reverse(i,j,s);
+    }
+
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        reverse(i,j,s);
+        for(int k=0;k<s.length;k++){
+            System.out.print(s[k]);
+        }
+    }
+}
