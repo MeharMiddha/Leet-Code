a45b544450db2026a2c763f7a2fb9b6699f47d70
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2024-03-14 19:08:12 +0530

    Create tabulationApproach.java

---
+++ 983. Minimum Cost For Tickets/tabulationApproach.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int solveTab(int[] days,int[] costs,int index){
+        int n=days.length;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int option1=costs[0]+dp[i+1];
+            int j;
+            for(j=i;j<n && days[j]<days[i]+7;j++);
+            int option2=costs[1]+dp[j];
+            for(j=i;j<n && days[j]<days[i]+30;j++);
+            int option3=costs[2]+dp[j];
+            dp[i]=Math.min(option1,Math.min(option2,option3));
+        }
+        return dp[0];
+    }
+
+    public int mincostTickets(int[] days, int[] costs) {
+        // Tabulation Approach
+        return solveTab(days,costs,0);
+    }
+}
