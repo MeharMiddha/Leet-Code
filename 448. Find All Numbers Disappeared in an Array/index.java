09745efb3f9b5231ec8156173ea2c814c702c055
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2024-03-15 20:42:22 +0530

    Create index.java

---
+++ 448. Find All Numbers Disappeared in an Array/index.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int highest=nums.length;
+        List<Integer> list = new ArrayList<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums) set.add(i);
+        for(int i=1;i<=highest;i++){
+            if(!set.contains(i)) list.add(i);
+        }
+        return list;
+    }
+}
