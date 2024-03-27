929d5b66cf57d6d7b5e186d1355bcc753fd83060
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2024-03-27 18:13:20 +0530

    Create index.java

---
+++ 1662. Check If Two String Arrays are Equivalent/index.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String sb="";
+        String ans="";
+        for(String t:word1){
+            sb+=t;
+        }
+        for(String s:word2){
+            ans+=s;
+        } 
+        return sb.equals(ans);
+    }
+}
